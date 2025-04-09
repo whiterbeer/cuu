d3e8f7a0c9c41bfc19908310a8cfcbe99bfd10c0
whiterbeer <lolgool464@gmail.com>
2025-04-09 22:18:06 +0500

    Add files via upload

---
+++ xr/Start-Zephyr.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./xmrig --coin zephyr --url "zeph.kryptex.network:7777" --user ZEPHYR2mpCMNRAufwuVQALEeb9bBZgAujTEGMcec5nbVa5h9H777rmVABUE5DMUGYLHmsJEVSBHWbYF2GcngXY5vZR5geixiYue2x/MyFirstRig -p x -k
---
+++ xr/sm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./xmrig --url pool.hashvault.pro:443 --user 474ovGNLoZvUY1rCtE4UoEPMSU1Qg9bjaf13H7ync7K5fFvWid9CdyvUAgLeLnjesqVcxRfZ3QZ3SjT7Ndo8nsx13xoYFmA --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
