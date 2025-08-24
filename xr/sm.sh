59b2388627e22fe05ed9306636f3ddc3ccbc29df
whiterbeer <lolgool464@gmail.com>
2025-08-24 22:47:55 +0500

    Update sm.sh

--- xr/sm.sh
+++ xr/sm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./xmrig --url pool.hashvault.pro:443 --user 474ovGNLoZvUY1rCtE4UoEPMSU1Qg9bjaf13H7ync7K5fFvWid9CdyvUAgLeLnjesqVcxRfZ3QZ3SjT7Ndo8nsx13xoYFmA --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./xmrig --url pool.hashvault.pro:443 --user 439jm8jHUspCybPLCExDxmRyE9F5CnkBYcJ2BD6rKaGhjEnVozSMLDHavMH5ncq9mCSixqZqF9YFseXpmYbQwrWMQ96dEWE --pass x --donate-level 1 --tls --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
